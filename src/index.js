a78fe818668805c578b4b7ed322ad0f6bc4b6910
Johannes Intke <124790333+intkej@users.noreply.github.com>
2024-05-24 11:20:22 +0300

    Add get-method to index.js
    Needs an rate limit system right now

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('vesapi')
+})
+
+app.get('/api/get-temperature', (req, res) => {
+  res.json({ message: 'Get temperature' })
 })
 
 app.listen(process.env.SERVER_PORT, () => {
