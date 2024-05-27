095c22bd3c87e80a91fd8faed5793c7e53492c35
Ronkkeli <contact@ronkkeli.com>
2024-05-27 15:04:42 +0300

    Ratelimit
    Ratelimit lisätty -> /api/get-temperature käyttämällä express-rate-limit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 require('dotenv').config();
-const express = require('express')
-const app = express()
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const app = express();
 
 app.use(express.json());
 
+
+const ratelimit = rateLimit({
+  windowMs: 1 * 60 * 1000, 
+  max: 1, 
+  message: 'Liian monta pyyntöä tältä IP:ltä, yritä uudelleen minuutin kuluttua'
+});
+
+
+app.use('/api/get-temperature', ratelimit);
+
 app.get('/', (req, res) => {
-  res.send('vesapi')
-})
+  res.send('vesapi');
+});
 
 app.get('/api/get-temperature', (req, res) => {
-  res.json({ message: 'Get temperature' })
-})
+  res.json({ message: 'Hae lämpötila' });
+});
 
 app.listen(process.env.SERVER_PORT, () => {
-  console.log('vesidata-api listening on port:', process.env.SERVER_PORT)
-})
+  console.log('vesidata-api kuuntelee portissa:', process.env.SERVER_PORT);
+});
