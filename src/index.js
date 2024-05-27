c36a567754c2acd03b0b0545fcfe3c8a893658a6
Ronkkeli <contact@ronkkeli.com>
2024-05-27 15:21:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-require('dotenv').config();
-const express = require('express');
-const rateLimit = require('express-rate-limit');
+require("dotenv").config();
+const express = require("express");
+const rateLimit = require("express-rate-limit");
 const app = express();
 
 app.use(express.json());
 
-
 const ratelimit = rateLimit({
-  windowMs: 1 * 60 * 1000, 
-  max: 1, 
-  message: 'Liian monta pyyntöä tältä IP:ltä, yritä uudelleen minuutin kuluttua'
+  windowMs: 1 * 60 * 1000,
+  max: 1,
+  message:
+    "Liian monta pyyntöä tältä IP:ltä, yritä uudelleen minuutin kuluttua",
 });
 
+app.use("/api/get-temperature", ratelimit);
 
-app.use('/api/get-temperature', ratelimit);
-
-app.get('/', (req, res) => {
-  res.send('vesapi');
+app.get("/", (req, res) => {
+  res.send("vesapi");
 });
 
-app.get('/api/get-temperature', (req, res) => {
-  res.json({ message: 'Hae lämpötila' });
+app.get("/api/get-temperature", (req, res) => {
+  res.json({ message: "Hae lämpötila" });
 });
 
 app.listen(process.env.SERVER_PORT, () => {
-  console.log('vesidata-api kuuntelee portissa:', process.env.SERVER_PORT);
+  console.log("vesidata-api kuuntelee portissa:", process.env.SERVER_PORT);
 });
